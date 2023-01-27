59618e7654bc2bf6b6be86f4d4c3590ba1b151e3
RohithS1729 <rohi741@gmail.com>
2023-01-27 14:30:08 +0530

    content reaction merge

---
+++ service_content/services/getVotesService.js
@@ -0,0 +1,20 @@
+const OptionData=require("../modals/optionsData")
+
+const getVotesService=(req,res)=>{
+    OptionData.find({    
+        postId:req.query.postId
+    }).exec((err,data)=>{
+        if(err){
+            res.send({err})
+        }
+        else if(data.length>0){
+            // let count=data.length
+            res.send(data)
+        }
+        else{
+            console.log(data)
+            res.send('0')
+        }       
+    })
+}
+module.exports=getVotesService
