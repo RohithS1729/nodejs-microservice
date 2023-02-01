1d7ba58e3eb78206175c3cb7d7d6200f3aaaea84
RohithS1729 <rohi741@gmail.com>
2023-02-01 11:16:22 +0530

    adding try and catch

--- service_content/services/deleteReactions.js
+++ service_content/services/deleteReactions.js
@@ -1,24 +1,29 @@
 const ReactionData=require('../modals/reactionsData')
 
 const deleteReactions=(req,res)=>{
-    ReactionData.findOneAndDelete({$and:[
-        {likedBy:req.query.likedBy},
-        {postId:req.query.postId}
-    ]}).exec((err,data)=>{
-        if(err){
-            res.send({err})
-        }else if(!data){
-            res.send({
-                msg:'no reaction from that user found'
-            })
-        }else{
-            res.send({
-                msg:"deleted",
-                unliked:data
-            })
-        }
-
-        
-    })
+    try{
+        ReactionData.findOneAndDelete({$and:[
+            {likedBy:req.query.likedBy},
+            {postId:req.query.postId}
+        ]}).exec((err,data)=>{
+            if(err){
+                res.send({err})
+            }else if(!data){
+                res.send({
+                    msg:'no reaction from that user found'
+                })
+            }else{
+                res.send({
+                    msg:"deleted",
+                    unliked:data
+                })
+            }
+    
+            
+        })
+    }
+    catch(err){
+        res.send('error ================= in deleteReactions file',err)
+    }
 }
 module.exports=deleteReactions
--- service_users/services/loginService.js
+++ service_users/services/loginService.js
@@ -1,27 +1,32 @@
 const UserData=require("../modals/userData")
 
 const loginService=(req,res)=>{
-    UserData.findOne({username:req.body.username}).exec((err,data)=>{
-        if(err){
-            res.send(err)
-        }else if(!data){
-            res.send({
-                msg:'No such user found, please sign up!!'
-            })
-        }else if(data.comparePassword(req.body.password)){
+    try{
+        UserData.findOne({username:req.body.username}).exec((err,data)=>{
+            if(err){
+                res.send(err)
+            }else if(!data){
+                res.send({
+                    msg:'No such user found, please sign up!!'
+                })
+            }else if(data.comparePassword(req.body.password)){
+                
+                    res.send({
+                        msg:'logged in',
+                        userId:data._id
+                    })
+                }else{
+                    res.send({
+                        msg:'wrong password or username'
+                        
+                        
+                    })
+                }
             
-                res.send({
-                    msg:'logged in',
-                    userId:data._id
-                })
-            }else{
-                res.send({
-                    msg:'wrong password or username'
-                    
-                    
-                })
-            }
-        
-    })
+        })
+    }
+    catch(err){
+        res.send('error ================= in loginService',err)
+    }
 }
 module.exports=loginService
