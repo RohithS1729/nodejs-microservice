3c50cf35bea3b568a225c8783ce10118ae0ee7df
RohithS1729 <rohi741@gmail.com>
2023-02-08 16:01:02 +0530

    swagger

--- service_users/api-router/index.js
+++ service_users/api-router/index.js
@@ -5,7 +5,24 @@ const signUp=require('./signUp.route')
 const {userProfile}=require("../controllers/users.controller")
 
 
+
+
+
 router.use('/signUp',signUp)
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     description: Get all books
+ *     parameters:
+ *      - name: username
+ *        description: username
+ *        
+ *        required: true
+ *        type: string
+
+ */
 router.use('/login',login)
 router.use('/profile',userProfile)
 
--- service_users/server.js
+++ service_users/server.js
@@ -9,10 +9,10 @@ const app=express()
 app.use(cors())
 
 //image upload
-const fileUpload=require("express-fileupload")
-app.use(fileUpload({
-    useTempFiles:true
-}))
+// const fileUpload=require("express-fileupload")
+// app.use(fileUpload({
+//     useTempFiles:true
+// }))
 
 //body parser
 const bodyParser=require("body-parser")
@@ -26,6 +26,28 @@ const db= mongoose.connection;
 db.on('error',()=>{console.log('did not connect to db')});
 db.on('open',()=>{console.log('started listening to db')});
 
+//++++++++++++++++++++++++++++++++++++
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Library API",
+      version: '1.0.0',
+    },
+  },
+  apis: ["server.js","./api-router/index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++
 
 
 app.use(router)
